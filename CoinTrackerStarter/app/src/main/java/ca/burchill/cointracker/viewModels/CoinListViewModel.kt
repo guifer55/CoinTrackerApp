87f4659cb209acb4ef2ca0c1c4e0a55f27d6ca7f
guifer55 <gui.advaita@gmail.com>
2021-04-11 19:56:28 -0300

    Bitcoin app

---
+++ CoinTrackerStarter/app/src/main/java/ca/burchill/cointracker/viewModels/CoinListViewModel.kt
@@ -0,0 +1,79 @@
+package ca.burchill.cointracker.viewModels
+
+import android.app.Application
+import androidx.lifecycle.*
+import ca.burchill.cointracker.database.getDatabase
+import ca.burchill.cointracker.repository.CoinsRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+
+enum class CoinApiStatus { LOADING, ERROR, DONE }
+
+
+class CoinListViewModel(application: Application) : AndroidViewModel(application) {
+
+
+    private val coinsRepository = CoinsRepository(getDatabase(application))
+
+
+    val coins = coinsRepository.coins
+
+
+    private var _eventNetworkError = MutableLiveData<Boolean>(false)
+
+
+    val eventNetworkError: LiveData<Boolean>
+        get() = _eventNetworkError
+
+
+    private var _isNetworkErrorShown = MutableLiveData<Boolean>(false)
+
+
+
+    init {
+        refreshDataFromRepository()
+    }
+
+
+    private fun refreshDataFromRepository() {
+        viewModelScope.launch {
+            try {
+                coinsRepository.refreshCoins()
+                _eventNetworkError.value = false
+                _isNetworkErrorShown.value = false
+
+            } catch (networkError: IOException) {
+                // Show a Toast error message and hide the progress bar.
+                _status.value = CoinApiStatus.ERROR
+            }
+        }
+    }
+
+
+    class Factory(val app: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(CoinListViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return CoinListViewModel(app) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+    }
+
+
+    private val _status = MutableLiveData<CoinApiStatus>()
+    val status: LiveData<CoinApiStatus>
+        get() = _status
+
+    private var viewModelJob = Job()
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+}
